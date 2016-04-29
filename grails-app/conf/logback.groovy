a5234d94feafab38f349f12297ec4f38838bb62f
Hans Bogaards <hbogaards@gmail.com>
2016-04-29 16:58:47 +0200

    Build corrections

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,42 +1,23 @@
-import static ch.qos.logback.classic.Level.*
-
-import ch.qos.logback.classic.encoder.PatternLayoutEncoder
-import ch.qos.logback.classic.filter.ThresholdFilter
-import ch.qos.logback.core.ConsoleAppender
-import ch.qos.logback.core.FileAppender
-import ch.qos.logback.core.rolling.RollingFileAppender
-import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
-
-scan()
+import grails.util.BuildSettings
+import grails.util.Environment
 
 // See http://logback.qos.ch/manual/groovy.html for details on configuration
-appender('CONSOLE', ConsoleAppender) {
-    encoder(PatternLayoutEncoder) {
-        pattern = "%d{ISO8601}|%-5p|%-20.20t|%-32.32c{32}| %X%m%n"
-    }
-    filter(ThresholdFilter) {
-        level = INFO
-    }
-}
-
-appender('ROLLING_FILE', RollingFileAppender) {
-    file = 'logs/quercus-php.log'
-    encoder(PatternLayoutEncoder) {
-        pattern = "%d{ISO8601}|%-5p|%-20.20t|%-32.32c{32}| %X%m%n"
-    }
-    rollingPolicy(TimeBasedRollingPolicy) {
-        fileNamePattern = 'logs/quercus-php.log.%d{yyyy-MM-dd}.gz'
-        maxHistory = 14
-    }
-}
-
-root(DEBUG, ['ROLLING_FILE','CONSOLE'])
-
-appender("FULL_STACKTRACE", FileAppender) {
-    file = "logs/stacktrace.log"
-    append = true
+appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "%level %logger - %msg%n"
     }
 }
-logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+
+root(ERROR, ['STDOUT'])
+
+def targetDir = BuildSettings.TARGET_DIR
+if (Environment.isDevelopmentMode() && targetDir) {
+    appender("FULL_STACKTRACE", FileAppender) {
+            file = "${targetDir}/stacktrace.log"
+        append = true
+        encoder(PatternLayoutEncoder) {
+            pattern = "%level %logger - %msg%n"
+        }
+    }
+    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+}
